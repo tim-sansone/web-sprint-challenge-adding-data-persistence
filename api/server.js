6896961571f8aa847d3326a536bcb93a0ecaefe2
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-22 09:30:44 -0500

    initial commit server up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
-// build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
+const taskRouter = require('./task/router');
+
+server.use(express.json())
+server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
+server.use('./api/tasks', taskRouter)
+
+server.get('/', (req, res) => {
+    res.send('Hello from base URL')
+})
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({message: error.message || 'internal server error'})
+})
+
+module.exports = server;
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,5 @@
 // build your `/api/tasks` router here
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
